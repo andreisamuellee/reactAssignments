378fbfa01cc4bb3f74efe0076b5f9e6968f5e687
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-03-18 00:19:33 +0200

    1 not done

---
+++ src/components/MediaTable.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import MediaRow from './MediaRow'
+import PropTypes from 'prop-types';
+
+export default function MediaTable({ picArray }) {
+    return (
+        <table>
+            <tbody>
+                {
+                    picArray.map((item, index) => (<MediaRow key={index} file={item} />))
+                }
+            </tbody>
+        </table>
+    )
+}
+
+MediaTable.propTypes = {
+    picArray: PropTypes.array,
+};
