798e83e89d9fe49131f66d7fd8ec29696e818293
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-03-18 13:50:35 +0200

    first app

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -3,17 +3,17 @@ import MediaRow from './MediaRow'
 import PropTypes from 'prop-types';
 
 export default function MediaTable({ picArray }) {
-    return (
-        <table>
-            <tbody>
-                {
-                    picArray.map((item, index) => (<MediaRow key={index} file={item} />))
-                }
-            </tbody>
-        </table>
-    )
+  return (
+    <table>
+      <tbody>
+        {
+          picArray.map((item, index) => (<MediaRow key={index} file={item} />))
+        }
+      </tbody>
+    </table>
+  )
 }
 
 MediaTable.propTypes = {
-    picArray: PropTypes.array,
+  picArray: PropTypes.array,
 };
