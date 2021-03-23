868df94ff18703972ac0ee787c2ce5f8165c5999
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-03-23 13:24:56 +0200

    ajax

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import MediaRow from './MediaRow';
-import { useState, useEffect } from 'react';
-import { baseUrl } from '../utils/variables';
+import { useAllMedia } from '../hooks/apiHooks';
 
 export default function MediaTable() {
-  const [picArray, setPicArray] = useState([]);
-
-  useEffect(() => {
-    const loadMedia = async () => {
-      const response = await fetch(baseUrl + 'media');
-      const files = await response.json();
-      console.log(files);
-
-      const media = await Promise.all(files.map(async (item) => {
-        const resp = await fetch(baseUrl + 'media/' + item.file_id)
-        return resp.json();
-      }));
-
-      setPicArray(media);
-    };
-
-    loadMedia();
-  }, []);
+  const picArray = useAllMedia();
 
   return (
     <table>
