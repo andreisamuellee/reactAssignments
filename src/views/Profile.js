67f62dd671ccafe43b324990785d67654871ef12
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-03-31 22:07:30 +0300

    contexts

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,7 +1,19 @@
+import { useContext } from 'react';
+import { MediaContext } from '../contexts/MediaContext';
+
 const Profile = () => {
+  const [user] = useContext(MediaContext);
+
   return (
     <>
       <h1>Profile</h1>
+      {user &&
+        <div>
+          <p>{user.full_name}</p>
+          <p>{user.email}</p>
+          <p>{user.username}</p>
+        </div>
+      }
     </>
   );
 };
