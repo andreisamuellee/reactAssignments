c897395703a925b62cb812997da693878c648351
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-04-06 12:55:34 +0300

    andrei

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,24 +1,28 @@
-import useSignUpForm from '../hooks/RegisterHooks';
+import useForm from '../hooks/FormHooks';
 import { useUsers } from '../hooks/ApiHooks';
-import { Grid, TextField, Typography, Button } from '@material-ui/core';
+import { Grid, Typography, Button } from '@material-ui/core';
 // import {useState} from 'react';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const RegisterForm = () => {
+const RegisterForm = ({ setToggle }) => {
   const { register, getUserAvailable } = useUsers();
   const validators = {
     username: ['required', 'minStringLength: 3', 'isAvailable'],
-    password: false,
-    email: false,
-    full_name: false,
+    password: ['required', 'minStringLength:5'],
+    confirm: ['required', 'isPasswordMatch'],
+    email: ['required', 'isEmail'],
+    // eslint-disable-next-line max-len
+    full_name: ['matchRegexp:^[a-zA-ZåäöÅÄÖ]+(([\',. -][a-zA-ZåäöÅÄÖ ])?[a-zA-ZåäöÅÄÖ]*)*$'],
   };
 
   const errorMessages = {
     username: ['vaadittu kenttä', 'vähintään 3 merkkiä', 'tunnus ei oo vapaa'],
-    password: 'salasana väärä muotoa',
-    email: 'sähköposti väärää muotoa',
-    full_name: 'vain kirjamia siis hei pliis jooko',
+    password: ['vaadittu kenttä', 'vähintään 5 merkkiä'],
+    confirm: ['vaadittu kenttä', 'salasanat eivät täsmää'],
+    email: ['vaadittu kenttä', 'sähköposti väärää muotoa'],
+    full_name: ['vain kirjamia siis hei pliis jooko'],
   };
 
   const doRegister = async () => {
@@ -27,12 +31,25 @@ const RegisterForm = () => {
       const available = await getUserAvailable(inputs.username);
       console.log('availabale', available);
       if (available) {
-        register(inputs);
+        delete inputs.confirm;
+        const result = await register(inputs);
+        if (result.message.length > 0) {
+          alert(result.message);
+          setToggle(true);
+        }
       }
     } catch (e) {
       console.log(e.message);
     }
   };
+
+  const { inputs, handleInputChange, handleSubmit } = useForm(doRegister, {
+    username: '',
+    password: '',
+    confirm: '',
+    email: '',
+    full_name: '',
+  });
 
   useEffect(() => {
     ValidatorForm.addValidationRule('isAvailable', async (value) => {
@@ -47,9 +64,13 @@ const RegisterForm = () => {
         }
       }
     });
-  }, []);
 
-  const { inputs, handleInputChange, handleSubmit } = useSignUpForm(doRegister);
+    ValidatorForm.addValidationRule('isPasswordMatch',
+      (value) => (value === inputs.password),
+    );
+  }, [inputs]);
+
+
   // console.log('RegisterForm', inputs);
 
   return (
@@ -77,35 +98,54 @@ const RegisterForm = () => {
             </Grid>
 
             <Grid container item>
-              <TextField
+              <TextValidator
                 fullWidth
                 type="password"
                 name="password"
                 label="Password"
                 onChange={handleInputChange}
                 value={inputs.password}
+                validators={validators.password}
+                errorMessages={errorMessages.password}
               />
             </Grid>
 
             <Grid container item>
-              <TextField
+              <TextValidator
+                fullWidth
+                type="password"
+                name="confirm"
+                label="Confirm password"
+                onChange={handleInputChange}
+                value={inputs.confirm}
+                validators={validators.confirm}
+                errorMessages={errorMessages.confirm}
+              />
+            </Grid>
+
+            <Grid container item>
+              <TextValidator
                 fullWidth
                 type="email"
                 name="email"
                 label="Email"
                 onChange={handleInputChange}
                 value={inputs.email}
+                validators={validators.email}
+                errorMessages={errorMessages.email}
               />
             </Grid>
 
             <Grid container item>
-              <TextField
+              <TextValidator
                 fullWidth
                 type="text"
                 name="full_name"
                 label="Full name"
                 onChange={handleInputChange}
                 value={inputs.full_name}
+                validators={validators.full_name}
+                errorMessages={errorMessages.full_name}
               />
             </Grid>
 
@@ -122,6 +162,10 @@ const RegisterForm = () => {
       </Grid>
     </Grid>
   );
+};
+
+RegisterForm.propTypes = {
+  setToggle: PropTypes.func,
 };
 
 export default RegisterForm;
