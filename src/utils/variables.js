c0ca99eff191769a911c70c9215cca7912a862a3
andreisamuellee <54439182+andreisamuellee@users.noreply.github.com>
2021-04-06 12:50:19 +0300

    andrei

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,4 +1,4 @@
 const baseUrl = 'https://media-new.mw.metropolia.fi/wbma/';
-const uploadsUrl = 'http://media-new.mw.metropolia.fi/wbma/uploads/';
+const uploadsUrl = 'https://media-new.mw.metropolia.fi/wbma/uploads/';
 
 export { baseUrl, uploadsUrl };
